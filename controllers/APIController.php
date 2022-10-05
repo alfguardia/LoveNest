d377d7c272979e9466c4a8663a4c5f06a97f67cc
alfguardia <alfguardia@hotmail.com>
2022-10-05 15:24:13 -0300

    Error mostrando gastos

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -68,7 +68,7 @@ class APIController
         $gastos->usuarioId = $usuarioId;
         $resultado = $gastos->orderBy('categoria', $gastos->$usuarioId);
 
-        echo json_encode($resultado);
+        echo json_encode('Enviando resultados...');
     }
 
     public static function editarGasto()
