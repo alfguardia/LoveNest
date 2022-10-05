f080f89034272fff7edaaff9fae6e1386190f631
alfguardia <alfguardia@hotmail.com>
2022-10-05 15:09:23 -0300

    Error guardado gastos

--- src/js/gastos.js
+++ src/js/gastos.js
@@ -8,6 +8,7 @@ let url = window.location.href.split('/');
 let gastos = [];
 // Listeners
 document.addEventListener('DOMContentLoaded', function () {
+    console.log(url[4]);
     if (url[4] === 'lista-gastos') {
         obtenerGastos();
     }
