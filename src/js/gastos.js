cabcfa90f1f47104826228962aabdb03bf5e72f1
alfguardia <alfguardia@hotmail.com>
2022-10-05 14:54:11 -0300

    Error guardado gastos

--- src/js/gastos.js
+++ src/js/gastos.js
@@ -77,7 +77,7 @@ function confirmacion() {
 async function guardarGastos() {
     try {
         const data = new FormData(gastosForm);
-        const url = 'https://lovenest-app.herokuapp.com/registrar-gastos'
+        const url = 'https://lovenest-app.herokuapp.com/API/registrar-gastos'
         const resultado = await fetch(url, {
             method: 'POST',
             body: data
