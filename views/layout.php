38db1f3e6ca85d210b7fdccb890e537f9488bf68
alfguardia <alfguardia@hotmail.com>
2022-10-06 14:42:30 -0300

    Test

--- views/layout.php
+++ views/layout.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="build/css/app.css">
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="build/img/logo.ico" />
 </head>
 
 <body>
