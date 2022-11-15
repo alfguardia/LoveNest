a8e0e06b1468f8d4feb50a2ec8a5079d45569b2e
alfguardia <alfguardia@hotmail.com>
2022-11-15 13:22:21 -0300

    PWA Test

--- views/layout.php
+++ views/layout.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LoveNest</title>
-    <link rel="manifest" href="/manifest.json">
-    <script src="../test.js"></script>
+    <link rel="manifest" href="manifest.json">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -15,6 +14,7 @@
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <link rel="shortcut icon" href="build/img/logo.ico" />
+    
 </head>
 
 <body>
