7d1fc3557587355a8f90734c486c0e35d9ddd7c5
alfguardia <alfguardia@hotmail.com>
2022-11-23 15:13:07 -0300

    Router

--- Router.php
+++ Router.php
@@ -30,7 +30,7 @@ class Router
         //$currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
         // -----------------------------------------------
 
-        $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
+        $currentUrl = $_SERVER['REQUEST_URI'];
         $method = $_SERVER['REQUEST_METHOD'];
 
         $splitURL = explode('?',$currentUrl);
